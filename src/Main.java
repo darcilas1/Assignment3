d08d2d31609e2a8d2bd354b17f39c689a192ca48
darcilas1 <danielarcila48@gmail.com>
2024-05-10 23:18:45 -0500

    codigo para inmprmir nombre

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
+    public static void main(String[] args) {
+        printAssignmentMessage();
+    }
+
+    public static void printAssignmentMessage() {
+        System.out.println("Asignación 3 por Daniel Arcila Salazar");
+    }
 }
